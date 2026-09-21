211b2e9d0197ff0d6d17e1b1fae5d4dedb73f646
Reiner Sibaja Matarrita <rsibajam@gmail.com>
2026-09-21 09:18:41 -0600

    Update histograma-raster.R

--- visualizacion/histograma-raster.R
+++ visualizacion/histograma-raster.R
@@ -10,54 +10,32 @@
 # documentacion es basica.
 # ============================================================
 
-# Carga las librerias necesarias
-library(raster)
-library(rasterVis)
-library(rgdal)
-library(jpeg)
+library(raster)    #carga la libreria para trabajar con rasters
+library(rasterVis) #carga la libreria para visualizacion de rasters
+library(rgdal)     #carga la libreria para archivos espaciales
+library(jpeg)      #carga la libreria para imagenes
 
-# Define la carpeta de trabajo
-setwd("ruta/a/tus/datos")  # <-- ajustar a tu ruta real
+setwd("ruta/a/tus/datos") #directorio donde se encuentren los datos
 
-# Lista todos los archivos .tif de la carpeta "raster"
-listfiles <- list.files(path = "raster", pattern = ".tif", full.names = TRUE)
+listfiles <- list.files(path = "raster", pattern = ".tif", full.names = TRUE) #carga lista de raster de la carpeta
+ET <- raster(listfiles) #convierte la lista de archivos en un raster
+plot(ET, col = terrain.colors(20)) #grafica el raster con colores de terreno
+print(ET)
+ncell(ET) #obtiene el numero de celdas del raster
 
-# Abre el raster
-r <- raster(listfiles)
-print(r)
+#Histograma
+graf <- hist(ET, main = " ", breaks = 20, xlab = "Valor", ylab = "Frecuencia",
+             col = "black", maxpixels = ncell(ET), border = "white", plot = TRUE,
+             xlim = c(0,9), ylim = c(0,1300000)) #grafica la distribucion de valores del raster
 
-# Muestra cuantas celdas tiene el raster
-ncell(r)
+#Figura con escala de colores abajo########################################################
 
-# ------------------------------------------------------------
-# Histograma
-# ------------------------------------------------------------
-# Grafica la distribucion de valores del raster
-hist(r,
-     main = " ",
-     breaks = 20,              # numero de barras
-     xlab = "Valor",           # etiqueta eje X
-     ylab = "Frecuencia",      # etiqueta eje Y
-     col = "black",            # color de las barras
-     maxpixels = ncell(r),     # usa todas las celdas
-     border = "white",         # borde de las barras
-     plot = TRUE,
-     xlim = c(0, 9),           # rango eje X (ajustar)
-     ylim = c(0, 1300000))     # rango eje Y (ajustar)
+breaks <- seq(0, 9, by=0.5) #rango de colores en la figura
+cols <- colorRampPalette(c("green", "yellow", "red"))(length(breaks)-1) #rampa de colores
 
-# ------------------------------------------------------------
-# Mapa con escala de colores
-# ------------------------------------------------------------
-# Define los intervalos de la escala
-breaks <- seq(0, 9, by = 0.5)
+levelplot(ET, at=breaks, col.regions=cols, main = "Raster", letters= 5,
+          xlim=c( 649995, 730005), ylim=c( 1120005, 1180005)) #grafica el raster con la escala de colores
 
-# Crea una rampa de colores (verde -> amarillo -> rojo)
-cols <- colorRampPalette(c("green", "yellow", "red"))(length(breaks) - 1)
-
-# Grafica el raster con la escala personalizada
-levelplot(r,
-          at = breaks,
-          col.regions = cols,
-          main = "Raster",
-          xlim = c(649995, 730005),      # limites geograficos (ajustar)
-          ylim = c(1120005, 1180005))
+###########################################################################################
+plot(ET, breaks) #grafica el raster con los breaks definidos
+levelplot(ET, xlim=c( 649995, 730005), ylim=c( 1120005, 1180005)) #grafica el raster con los limites definidos
